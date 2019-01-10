fffba77f0e959ebf81f3d4bcaa2fdec7cde27891
Mukhin Igor <mukhinID@outlook.com>
2019-01-10 20:42:28 +0700

    SoundButton sprite swap.

--- Assets/Scripts/Buttons/SoundButton.cs
+++ Assets/Scripts/Buttons/SoundButton.cs
@@ -8,10 +8,24 @@ namespace Escapa.Buttons
     [RequireComponent(typeof(Button), typeof(Image))]
     public sealed class SoundButton : MonoBehaviour, IButton
     {
-        public void Action() => _systemController.IsSoundEnabled = !_systemController.IsSoundEnabled;
+        public Sprite SpriteOn;
+        public Sprite SpriteOff;
+
+        public void Action()
+        {
+            _systemController.IsSoundEnabled = !_systemController.IsSoundEnabled;
+            _image.sprite = _systemController.IsSoundEnabled ? SpriteOn : SpriteOff;
+        }
 
         private ISystemController _systemController;
+        private Image _image;
 
-        private void Awake() => _systemController = GameObject.FindWithTag(Tags.SystemController).GetComponent<ISystemController>();
+        private void Awake()
+        {
+            _systemController = GameObject.FindWithTag(Tags.SystemController).GetComponent<ISystemController>();
+            _image = GetComponent<Image>();
+        }
+
+        private void Start() => _image.sprite = _systemController.IsSoundEnabled ? SpriteOn : SpriteOff;
     }
 }
