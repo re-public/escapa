509068296154fe387996728c6f612b115687001a
Mukhin Igor <mukhinID@outlook.com>
2019-02-02 19:15:46 +0300

    Control improvements

--- Assets/Scripts/Units/Player.cs
+++ Assets/Scripts/Units/Player.cs
@@ -21,6 +21,16 @@ namespace Escapa.Units
         public float MovingTime => ScoreManager.CurrentRecord - _moveTimeStart;
 
         private SpriteRenderer _spriteRenderer;
+
+        private static float _unitsPerPixel;
+        private float _halfScreenHeight;
+        private float _halfScreenWidth;
+
+        private bool _isTouched;
+        private Vector3 _targetPosition;
+        private Vector3 _offset;
+        private Touch _touch;
+
         private float _idleTimeStart;
         private float _moveTimeStart;
 
@@ -30,55 +40,77 @@ namespace Escapa.Units
 
             _idleTimeStart = 0f;
             _moveTimeStart = 0f;
+            _isTouched = false;
+
+            _unitsPerPixel = Camera.main.orthographicSize * 2.0f / Screen.height;
+            _halfScreenHeight = Screen.height / 2.0f;
+            _halfScreenWidth = Screen.width / 2.0f;
         }
 
         private void Update()
         {
             if (Input.touchCount > 0)
-                OnTouch(Input.GetTouch(0));
+            {
+                _touch = Input.GetTouch(0);
+                OnTouch(_touch.phase, _touch.position);
+            }
+
+            if (_isTouched)
+            {
+                _targetPosition.x -= _offset.x;
+                _targetPosition.y -= _offset.y;
+
+                transform.position = _targetPosition;
+            }
         }
 
         private void OnCollisionEnter2D(Collision2D collision) => Die?.Invoke();
 
-        private void OnTouch(Touch touch)
+        private void OnTouch(TouchPhase phase, Vector2 position)
         {
-            Vector2 targetPosition = Camera.main.ScreenToWorldPoint(touch.position);
-            if (!IsTouched(transform.position, targetPosition)) return;
+            _targetPosition.x = (position.x - _halfScreenWidth) * _unitsPerPixel;
+            _targetPosition.y = (position.y - _halfScreenHeight) * _unitsPerPixel;
 
-            MousePressed?.Invoke();
-
-            switch (touch.phase)
+            switch (phase)
             {
                 case TouchPhase.Began:
+                    _isTouched = IsTouched(transform.position, _targetPosition);
+
+                    _offset.x = _targetPosition.x - transform.position.x;
+                    _offset.y = _targetPosition.y - transform.position.y;
                     break;
+
                 case TouchPhase.Moved:
                     if (_moveTimeStart < float.Epsilon)
                         _moveTimeStart = ScoreManager.CurrentRecord;
                     if (_idleTimeStart > float.Epsilon)
                         _idleTimeStart = 0f;
-
-                    transform.position = targetPosition;
-
                     break;
+
                 case TouchPhase.Stationary:
                     if (_idleTimeStart < float.Epsilon)
                         _idleTimeStart = ScoreManager.CurrentRecord;
                     if (_moveTimeStart > float.Epsilon)
                         _moveTimeStart = 0f;
+                    break;
 
-                    break;
                 case TouchPhase.Ended:
-                    break;
                 case TouchPhase.Canceled:
+                    _isTouched = false;
                     break;
+
                 default:
                     break;
             }
+
+            if (_isTouched)
+                MousePressed?.Invoke();
         }
 
-        private static bool IsTouched(Vector2 position, Vector2 touchPosition) => position.x - 0.75f < touchPosition.x
+        private static bool IsTouched(Vector3 position, Vector3 touchPosition) => position.x - 0.75f < touchPosition.x
                                                                                && touchPosition.x < position.x + 0.75f
                                                                                && position.y - 0.75f < touchPosition.y
                                                                                && touchPosition.y < position.y + 0.75f;
+
     }
 }
