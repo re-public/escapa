5ce405b9274b8c64c84d3478226d6836fc7e15c2
Mukhin Igor <mukhinid@outlook.com>
2021-07-11 15:25:23 +0300

    Added null checking.

--- sources/Assets/Scripts/Game/MainCamera.cs
+++ sources/Assets/Scripts/Game/MainCamera.cs
@@ -10,17 +10,30 @@ namespace Escapa.Game
     [RequireComponent(typeof(Camera))]
     public sealed class MainCamera : MonoBehaviour, IMainCamera
     {
-        public float UnitsPerPixel => camera.orthographicSize * 2.0f / Screen.height;
+        public float UnitsPerPixel { get; private set; }
 
-        public Vector2 ScreenToWorldPoint(Vector2 point) => camera.ScreenToWorldPoint(point);
-            
-        private new Camera camera;
+        public Vector2 ScreenToWorldPoint(Vector2 point) => Camera.ScreenToWorldPoint(point);
+
+        private Camera _camera;
+        private Camera Camera
+        {
+            get
+            {
+#if DEBUG
+                if (_camera == null)
+                    throw new InvalidOperationException();
+#endif
+                return _camera;
+            }
+            set => _camera = value;
+        }
 
         private void Awake()
         {
             DontDestroyOnLoad(gameObject);
 
-            camera = GetComponent<Camera>();
+            Camera = GetComponent<Camera>();
+            UnitsPerPixel = Camera.orthographicSize * 2.0f / Screen.height;
         }
 
         private void OnEnable()
@@ -35,12 +48,12 @@ namespace Escapa.Game
             SceneManager.sceneLoaded -= OnSceneLoaded;
         }
 
-        private void OnDifficultyChanged(object sender, EventArgs e) => camera.backgroundColor = StyleManager.Colors.Background;
+        private void OnDifficultyChanged(object sender, EventArgs e) => Camera.backgroundColor = StyleManager.Colors.Background;
 
         private void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
         {
             if (scene.buildIndex != (int)GameScenes.Preload)
-                camera.backgroundColor = StyleManager.Colors.Background;
+                Camera.backgroundColor = StyleManager.Colors.Background;
         }
     }
 }
